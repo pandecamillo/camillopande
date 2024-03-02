0238f9aa4cbbf6f2606066afbd401e072e85ccc1
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-03-02 20:33:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Pande Camillo
+    </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Ce site web a été créer pour publier mes cours de programmation et de vous permettre à les télécharger gratuitement
+    </p>
+        <p>
+          Ce site web est en cours de construction ... elle sera publié au mois d'avril
+    </p>
         <a
           className="App-link"
           href="https://reactjs.org"
